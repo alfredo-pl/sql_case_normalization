9d538527af16bdc77f99f13c0d79e975ad7b1ac2
alfredopl <aalfredoplz@gmail.com>
2020-11-18 23:29:44 -0300

    Completed case

--- case.sql
+++ case.sql
@@ -45,3 +45,118 @@ CREATE TABLE libro_lector (
     FOREIGN KEY (id_libro) REFERENCES libro(id), 
     FOREIGN KEY (id_lector) REFERENCES lector(id)
 );
+
+INSERT INTO editorial (id, nombre)
+VALUES (1,'UTHEA');
+INSERT INTO editorial (id, nombre)
+VALUES (2,'Reverté');
+INSERT INTO editorial (id, nombre)
+VALUES (3,'McGraw-Hill');
+INSERT INTO editorial (id, nombre)
+VALUES (4,'MIR');
+
+INSERT INTO libro(  id , titulo , editorial )
+VALUES (515,'Ecuaciones diferenciales para ingenieros y científicos',1);
+INSERT INTO libro(  id , titulo , editorial )
+VALUES (540,'Química',2);
+INSERT INTO libro(  id , titulo , editorial )
+VALUES (530,'Física',3);
+INSERT INTO libro(  id , titulo , editorial )
+VALUES (519,'Estadística',3);
+INSERT INTO libro(  id , titulo , editorial )
+VALUES (512,'Algebra Lineal',4);
+
+INSERT INTO autor(  id , nombre)
+VALUES (1,'Lambe C.G');
+INSERT INTO autor(  id , nombre)
+VALUES (2,'Tranter C.J');
+INSERT INTO autor(  id , nombre)
+VALUES (3,'Christen');
+INSERT INTO autor(  id , nombre)
+VALUES (4,'Hans Rudolf');
+INSERT INTO autor(  id , nombre)
+VALUES (5,'Serway');
+INSERT INTO autor(  id , nombre)
+VALUES (6,'Raymond A');
+INSERT INTO autor(  id , nombre)
+VALUES (7,'Murray Spiegel');
+INSERT INTO autor(  id , nombre)
+VALUES (8,'Voevodin');
+INSERT INTO autor(  id , nombre)
+VALUES (9,'Valentín V');
+
+INSERT INTO lector(  id , nombre)
+VALUES (1,'Pérez');
+INSERT INTO lector( id , nombre)
+VALUES (2,'Gómez');
+INSERT INTO lector( id , nombre)
+VALUES (3,'Juan');
+INSERT INTO lector( id , nombre)
+VALUES (4,'Ortega');
+INSERT INTO lector( id , nombre)
+VALUES (5,'Pereira');
+INSERT INTO lector( id , nombre)
+VALUES (6,'Margarita');
+INSERT INTO lector( id , nombre)
+VALUES (7,'García');
+INSERT INTO lector( id , nombre)
+VALUES (8,'Contreras');
+INSERT INTO lector( id , nombre)
+VALUES (9,'Rosa');
+INSERT INTO lector( id , nombre)
+VALUES (10,'López');
+INSERT INTO lector( id , nombre)
+VALUES (11,'Molina');
+INSERT INTO lector( id , nombre)
+VALUES (12,'Ana');
+INSERT INTO lector( id , nombre)
+
+INSERT INTO libro_autor(  id , codigo_libro, id_autor)
+VALUES (1,515,1);
+INSERT INTO libro_autor(  id , codigo_libro, id_autor)
+VALUES (2,515,2);
+INSERT INTO libro_autor(  id , codigo_libro, id_autor)
+VALUES (3,540,3);
+INSERT INTO libro_autor(  id , codigo_libro, id_autor)
+VALUES (4,540,4);
+INSERT INTO libro_autor(  id , codigo_libro, id_autor)
+VALUES (5,530,5);
+INSERT INTO libro_autor(  id , codigo_libro, id_autor)
+VALUES (6,530,6);
+INSERT INTO libro_autor(  id , codigo_libro, id_autor)
+VALUES (7,519,7);
+INSERT INTO libro_autor(  id , codigo_libro, id_autor)
+VALUES (8,512,8);
+INSERT INTO libro_autor(  id , codigo_libro, id_autor)
+VALUES (9,512,9);
+
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (1,515,1,'2019-09-15');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (2,515,2,'2019-09-15');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (3,515,3,'2019-09-15');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (4,540,4,'2019-08-23');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (5,540,5,'2019-08-23');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (6,540,6,'2019-08-23');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (7,530,7,'2019-09-24');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (8,530,8,'2019-09-24');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (9,530,9,'2019-09-24');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (10,519,10,'2019-09-18');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (11,519,11,'2019-09-18');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (12,519,12,'2019-09-18');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (13,512,1,'2019-09-17');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (14,512,2,'2019-09-17');
+INSERT INTO libro_lector( id, id_libro, id_lector, fecha_dev )
+VALUES (15,512,3,'2019-09-17');
